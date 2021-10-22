5dd75fc8a24b1a6337f63c158f215ca6f9dd3edc
BYAK <byakhee0314@gmail.com>
2021-10-22 16:52:06 +0900

    solve 223 b

--- abc223/src/bin/b.rs
+++ abc223/src/bin/b.rs
@@ -1,3 +1,17 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    }
+    let mut cases = Vec::new();
+    let mut current = s;
+    cases.push(format!("{}", current));
+    for _i in 1..current.len() {
+        current = format!("{}{}", &current[1..current.len()], &current[..1]);
+        cases.push(format!("{}", current));
+    }
+    cases.sort();
+    println!("{}", cases[0]);
+    println!("{}", cases[cases.len() - 1]);
 }
